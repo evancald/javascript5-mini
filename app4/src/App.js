87fb6a671b86057caf64016eb03fcff017e58cb2
Evan Caldwell <evancald@sas.upenn.edu>
2018-10-27 12:36:51 -0600

    fixed bugs

--- app4/src/App.js
+++ app4/src/App.js
@@ -8,22 +8,34 @@ class App extends Component {
     this.state = {
       cars: []
     }
+    this.getCars = this.getCars.bind(this);
   }
+
 
   getCars() {
     axios.get('https://joes-autos.herokuapp.com/api/vehicles')
     .then(res => {
       this.setState({
-        cars: res
+        cars: res.data
       })
     })
   }
 
   render() {
+    const cars = this.state.cars.map( (car, i) => {
+      return (
+        <div key={i}>
+          <div>Make: {car.make}</div>
+          <div>Model: {car.model}</div>
+          <div>Year: {car.year}</div>
+          <div>..........</div>
+        </div>
+      )
+    })
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+        {cars}
       </div>
     );
   }
